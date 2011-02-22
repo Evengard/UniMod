2eedea0100d6f942477e8ebae7a39a47989a3383
Evengard <Evengard@d8d557ae-dcdc-684f-8384-3c1aa70d208f>
2011-02-22 21:24:21 +0000

    фикс старого бага со спеллами закомментил отсутствующие инклуды

--- util.cpp
+++ util.cpp
@@ -961,16 +961,16 @@ void injectCon()
 	InjectJumpTo(0x00443C80,&onConCmd);// Функция реакции на консольную команду
 	InjectOffs(0x4D2AB5,&onEachFrame);
 
-#include "lua/binClient/clientOnJoin.lua.inc"
+//#include "lua/binClient/clientOnJoin.lua.inc"
 	lua_getfield(L,LUA_REGISTRYINDEX,"client");
 	lua_setfenv(L,-2);
 	lua_pcall(L,0,0,0);
-#include "lua/binServer/chatMode.lua.inc"
+//#include "lua/binServer/chatMode.lua.inc"
 	lua_getfield(L,LUA_REGISTRYINDEX,"server");
 	lua_setfenv(L,-2);
 	lua_pcall(L,0,0,0);
-#include "lua/binGlobal/dofile.lua.inc"
-#include "lua/binGlobal/filesystem.inc.lua.inc"
+//#include "lua/binGlobal/dofile.lua.inc"
+//#include "lua/binGlobal/filesystem.inc.lua.inc"
 
 	lua_settop(L,Top);
 
