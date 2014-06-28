51908dcc18ecf17698ba4b6919e2aafb8545e98e
Darkclainer <darkclainer@gmail.com>
2014-06-28 15:21:11 +0400

    Добавил тест на работу луа. Почему то не могу нормально прицепить либу от луа: нокс требует длл от луа.

--- UniMod2/init.cpp
+++ UniMod2/init.cpp
@@ -1,23 +1,45 @@
 #include <windows.h>
+#include "lua.hpp"
 #pragma pack(1)
-
+lua_State *globalL;
 
 
 BOOL WINAPI DllMain(HINSTANCE hInst, DWORD reason, LPVOID)
 {
-	if (reason == DLL_PROCESS_ATTACH)
-	{
-		MessageBox(
-			NULL,
-			L"HELLO WORLD!!!",
-			L"UniMod2",
-			MB_OK
-			);
-	}
-	if (reason == DLL_PROCESS_DETACH)
-	{
+	switch (reason) {
+		case DLL_PROCESS_ATTACH:
+		{
+			// тест
+			globalL = luaL_newstate();
 
-	}
+			if (luaL_dofile(globalL, "UniMod2.lua"))
+				break;
+			lua_getglobal(globalL, "prompt");
+			if (!lua_isstring(globalL, -1))
+			{
+				lua_pop(globalL, 1);
+				break;
+			}
+			const char *s = lua_tostring(globalL, -1);
+			int size = strlen(s) + 1;
+			wchar_t *ws = new wchar_t[size];
+			
+			mbstowcs(ws, s, size);
 
+			MessageBox(
+				NULL,
+				ws,
+				L"UniMod2",
+				MB_OK
+				);
+			break;
+
+			lua_pop(globalL, 1);
+			delete ws;
+		}
+		case DLL_PROCESS_DETACH:
+			lua_close(globalL);
+			break;
+	}
 	return 1;
 }
