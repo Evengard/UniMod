1554ce09c65f5291d0a0ada3b32da839e425eaec
Darkclainer <darkclainer@gmail.com>
2014-07-24 13:25:08 +0400

    lua51 теперь работает

---
+++ lua-5.1.5/src/lua.hpp
@@ -0,0 +1,9 @@
+﻿// lua.hpp
+// Lua header files for C++
+// <<extern "C">> not supplied automatically because Lua also compiles as C++
+
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
